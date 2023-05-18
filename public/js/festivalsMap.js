b0a8b199981a66302491caaf817697ee33158f55
Gustavo Gesto <gestogarciagustavo@gmail.com>
2023-05-18 15:15:05 +0200

    nada ya estamos otra vez subiendo toa la chicha mientras pedro hace el slider

--- public/js/festivalsMap.js
+++ public/js/festivalsMap.js
@@ -1,47 +1,27 @@
 let myMap
 
-function initViewMarkers() {
-    initMap()
-    getFestivalsJSON()
-}
+const festivalTitle = document.querySelector('#festivalTitle').value
+const festivalId = document.querySelector('#festivalID').value
+const festivalLat = document.querySelector('#festivalLat').value
+const festivalLon = document.querySelector('#festivalLon').value
 
+const festivalsCoords = { lat: Number(festivalLat), lng: Number(festivalLon) }
+
+function initViewMarkers() {
+
+    initMap()
+}
 
 function initMap() {
+
     myMap = new google.maps.Map(
         document.querySelector('#map'),
-        { zoom: 12, center: { lat: 40.392521370648154, lng: - 3.6989879718518366 }, }
+        { zoom: 12, center: { lat: parseFloat(festivalLat), lng: parseFloat(festivalLon) }, styles: mapStyles.aubergine }
     )
-}
-
-
-function getFestivalsJSON() {
-    fetch('/api/festivals')
-        .then(res => res.json())
-        .then(festivalsJSON => renderFestivalsMarkers(festivalsJSON))
-        .catch(err => console.log(err))
-}
-
-
-function renderFestivalsMarkers(festivalsJSON /* markerToMark */) {
-
-    festivalsJSON.forEach(elm => {
-
-        const festivalsCoords = { lat: elm.location.coordinates[1], lng: elm.location.coordinates[0] }
-
-        new google.maps.Marker({
-            map: myMap,
-            position: festivalsCoords,
-            title: elm.title,
-
-        })
-
-        // if (elm.id === markerToMark) {
-        //     new google.maps.Marker({
-        //         map: myMap,
-        //         position: festivalsCoords,
-        //         title: elm.title
-        //     });
-        // }
+    new google.maps.Marker({
+        map: myMap,
+        position: festivalsCoords,
+        title: festivalTitle
 
     })
 }
